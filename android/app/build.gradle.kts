632e23694a9d32b7da9bd77f5099eb65528c4ae6
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2025-09-03 03:57:04 +0300

    add configuration of Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.chatr"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,10 +25,12 @@ android {
         applicationId = "com.example.chatr"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
+
     }
 
     buildTypes {
@@ -38,7 +41,9 @@ android {
         }
     }
 }
-
+dependencies {
+  implementation("com.android.support:multidex:1.0.3")
+}
 flutter {
     source = "../.."
 }
