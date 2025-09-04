51d0515a3340aa6fe6918192c75f8b640dc30d93
mahmoudbakir2015 <mahmoudbakir2015@gmail.com>
2025-09-04 18:05:54 +0300

    fix test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:chatr/utils/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,8 +14,11 @@ import 'package:chatr/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    String? token = await TokenStorage.getToken();
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(token: token));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
